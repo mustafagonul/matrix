bb82d64bbd1f21ac8f8980581a78fd1243bb42a2
Mustafa Gonul <mustafagonul@gmail.com>
2017-12-22 20:46:04 +0300

    - Interface improved

--- matrix.hpp
+++ matrix.hpp
@@ -1,27 +1,40 @@
 #pragma once
 
-
 #include "matrix_traits.hpp"
 
 
-template <typename TYPE>
-class matrix : public matrix_traits<TYPE> {
+template <typename Type, template <typename> typename Traits = matrix_traits>
+class matrix {
+public:
+  using traits_t = Traits<Type>;
+  using matrix_t = matrix<Type, Traits>;
+
+  using type_t = typename traits_t::type_t;
+  using row_t  = typename traits_t::row_t;
+  using col_t  = typename traits_t::col_t;
+  using elem_t = typename traits_t::elem_t;
+  using imp_t  = typename traits_t::imp_t;
+
 public:
   matrix(row_t, col_t, elem_t = elem_t{});
 
 public:
-  auto get(row_t, col_t, row_t, col_t) const -> matrix_t;
   auto get(row_t, col_t) -> elem_t&;
   auto get(row_t, col_t) const -> elem_t;
+  auto get(row_t, col_t, row_t, col_t) const -> matrix_t;
+  
   auto set(row_t, col_t, elem_t const&) -> void;
+  auto set(row_t, col_t, matrix_t const&) -> void;
 
   auto add(matrix_t const&) -> void;
   auto sub(matrix_t const&) -> void;
+  auto mul(elem_t) -> void;
   auto dot(matrix_t const& ) const -> matrix_t;
+  auto tr() const -> matrix_t;
 
 private:
   row_t m_row;
-  col_t m_row;
+  col_t m_col;
   imp_t m_imp;
 };
 
--- matrix_traits.hpp
+++ matrix_traits.hpp
@@ -1,19 +1,17 @@
 #pragma once 
 
-
 #include "matrix_exception.hpp"
 #include <vector>
 
 
-template <typename TYPE>
+template <typename Type>
 class matrix_traits {
 public:
-  using type_t = TYPE;
+  using type_t = Type;
   using elem_t = type_t;
   using size_t = unsigned;
   using row_t = size_t;
   using col_t = size_t;
-  using matrix_t = Matrix<type_t>;
   using imp_t = std::vector<type_t>;
 
 };
